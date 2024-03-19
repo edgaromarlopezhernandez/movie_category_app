da46766c603e5c1dfb4c7b325d552565dbc47436
edgar <edgarlopez.hdez@gmail.com>
2024-03-19 17:09:36 -0600

    :tada:Begin Movie Category App

---
+++ src/main/java/com/movies/MovieCategoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCategoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCategoryAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/movies/utils/AppRunner.java
@@ -0,0 +1,14 @@
+package com.movies.utils;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Movie app started!");
+    }
+}
